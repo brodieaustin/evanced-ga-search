4ef2d1943261d993162f0d9934b9a6a7ed505ebf
brodieaustin <brodie.austin@gmail.com>
2014-07-15 23:04:14 -0500

    renamed ga-event file to ga-search

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task('minify', function(){
-    gulp.src('ga-events.js')
+    gulp.src('ga-search.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(''));
